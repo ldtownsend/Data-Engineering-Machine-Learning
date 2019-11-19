a75909d69b7ae0c08affc425e52900a5528954c9
xander-bennett <xander-bennett@users.noreply.github.com>
2019-11-18 17:52:09 -0800

    template app.py file

---
+++ app.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import numpy as np
+import pickle
+
+# app
+app = Flask(__name__)
+
+# routes
+@app.route('/', methods=['POST'])
+
+def predict():
+    # get data
+    data = request.get_json(force=True)
+
+    # convert data into dataframe
+    data.update((x, [y]) for x, y in data.items())
+    data_df = pd.DataFrame.from_dict(data)
+
+    # predictions
+    result = model.predict(data_df)
+
+    # send back to browser
+    output = {'results': int(result[0])}
+
+    # return data
+    return jsonify(results=output)
+
+if __name__ == '__main__':
+    app.run(port = 5000, debug=True)
+# pickle.dump(model, open(‘model.pkl’, ‘wb’))
