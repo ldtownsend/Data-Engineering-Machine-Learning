15b86b903196e7500aadab7c1488224940505dec
Pdugovich <pdugovich@Gmail.com>
2019-11-19 13:45:03 -0500

    Example framework for API

--- app.py
+++ app.py
@@ -1,30 +1,41 @@
 '''Example app.py file. TODO - '''
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify
 import pandas as pd
 import numpy as np
 import pickle
 
+
 # app
 app = Flask(__name__)
 
-# where the pickled models would go if we choose to pickle
-# pickle.dump(model, open(‘model.pkl’, ‘wb’))
+# Loading the model once it's available
+model = pickle.load(open('model.pkl', 'rb'))
 
 # routes
-@app.route('/', methods=['GET','POST'])
-def root():
-    """
-    Base for directory. Will return base.html template
-    """
-    header = 'Welcome!'
-    return render_template('base.html', message=header)
+@app.route('/', methods=['POST'])
+def predict():
+    if model:
+        try:
+            # Requesting JSON data
+            request_data = request.get_json(force=True)
 
-@app.route('/request', methods=[]'GET','POST'])
-def fetch_data():
-    """
-    Fetches data in a JSON format to be fed into predictive model
-    """
+            # Turning data into pandas DataFrame for use in model
+            request_data.update((x,[y]) for x,y in request_data.items())
+            data_df = pd.DataFrame.from_dict(request_data)
 
+            # Make prediction based on created dataframe of user input data
+            result = model.predict(data_df)
+
+            #Creating dict to convert to json and return
+            output = {'result': int(result[0])}
+
+            return jsonify(results=output)
+
+        except Exception as e:
+            return jsonify({'Error': e})
+
+    else:
+        return jsonify({'Error': 'No Model Available'})
 
 if __name__ == '__main__':
     app.run(debug=True)
