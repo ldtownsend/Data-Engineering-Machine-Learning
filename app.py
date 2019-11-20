3955188d30e05339a439f1252befa2b69e62c740
xander-bennett <xander-bennett@users.noreply.github.com>
2019-11-19 16:10:35 -0800

    added one-hot encoding to predict function

--- app.py
+++ app.py
@@ -19,6 +19,28 @@ def predict():
             # Requesting JSON data
             request_data = request.get_json(force=True)
 
+            # Performing the manual equivalent of one-hot encoding for the top 20 most popular amenities.
+            df['Wifi'] = df['amenities'].str.contains('Wifi')
+            df['Kitchen'] = df['amenities'].str.contains('Kitchen')
+            df['Heating'] = df['amenities'].str.contains('Heating')
+            df['Essentials'] = df['amenities'].str.contains('Essentials')
+            df['Hair dryer'] = df['amenities'].str.contains('Hair dryer')
+            df['Laptop friendly workspace'] = df['amenities'].str.contains('Laptop friendly workspace')
+            df['Hangers'] = df['amenities'].str.contains('Hangers')
+            df['Iron'] = df['amenities'].str.contains('Iron')
+            df['Shampoo'] = df['amenities'].str.contains('Shampoo')
+            df['TV'] = df['amenities'].str.contains('TV')
+            df['Hot water'] = df['amenities'].str.contains('Hot water')
+            df['Internet'] = df['amenities'].str.contains('Internet')
+            df['Host greets you'] = df['amenities'].str.contains('Host greets you')
+            df['Smoke detector'] = df['amenities'].str.contains('Smoke detector')
+            df['Buzzer/wireless intercom'] = df['amenities'].str.contains('Buzzer/wireless intercom')
+            df['Lock on bedroom door'] = df['amenities'].str.contains('Lock on bedroom door')
+            df['Buzzer/wireless intercom'] = df['amenities'].str.contains('Buzzer/wireless intercom')
+            df['Refrigerator'] = df['amenities'].str.contains('Refrigerator')
+            df['Free street parking'] = df['amenities'].str.contains('Free street parking')
+            df['Dishes and silverware'] = df['amenities'].str.contains('Dishes and silverware')
+
             # creating dataframe variable from dictionary
             df = pd.DataFrame.from_dict(x, orient='index').transpose()
 
